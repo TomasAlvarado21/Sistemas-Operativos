1a2c9b92b6fcf73f040271927e44f3f70a12d22e
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-09-29 00:03:19 -0300

    T3 ready

--- T3/disco.c
+++ T3/disco.c
@@ -8,15 +8,14 @@
 
 // Defina aca sus variables globales
 typedef struct {
-  char *nombre;
+  char *nombre_varon;
+  char *nombre_dama;
   int ready;
   pthread_cond_t w;
 } Request;
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
-int busyV = 0;
-int busyD = 0;
 
 Queue *Varones;
 Queue *Damas;
@@ -24,23 +23,56 @@ Queue *Damas;
 
 void DiscoInit(void) {
   // ... inicialice aca las variables globales ...
+  Varones = makeQueue();
+  Damas = makeQueue();
 }
 
 void DiscoDestroy(void) {
   // ... destruya las colas para liberar la memoria requerida ...
+  destroyQueue(Varones);
+  destroyQueue(Damas);
 }
 
 char *dama(char *nom) {
-  // ...
+  char *local_varon = NULL;
+  pthread_mutex_lock(&m);
+  if (!emptyQueue(Damas) || emptyQueue(Varones)){
+    Request req_dama = {"", nom , 0, PTHREAD_COND_INITIALIZER};
+    put(Damas, &req_dama);
+    while (!req_dama.ready){
+      pthread_cond_wait(&req_dama.w, &m);
+      local_varon = req_dama.nombre_varon;
+    }
+  }
+  else {
+    Request *req_varon = get(Varones);
+    req_varon->ready = 1;
+    local_varon = req_varon->nombre_varon; 
+    req_varon->nombre_dama=nom;
+    pthread_cond_signal(&req_varon->w);
+  }
+  pthread_mutex_unlock(&m);
+  return local_varon;
 }
 
 char *varon(char *nom) {
-  pthread_mutex_lock(&mutex);
-  if (!emptyQueue(Damas)) {
-    Request *nombre = get(Damas);
-    pthread_cond_signal(&nombre->w);
-    return dama;
+  char *local_dama = NULL;
+  pthread_mutex_lock(&m);
+  if(emptyQueue(Damas) || !emptyQueue(Varones)){
+    Request req_varon = {nom, "", 0, PTHREAD_COND_INITIALIZER};
+    put(Varones, &req_varon);
+    while (!req_varon.ready){
+      pthread_cond_wait(&req_varon.w, &m);
+      local_dama = req_varon.nombre_dama;
+    }
   }
-  
-  
+  else {
+    Request *req_dama = get(Damas);
+    req_dama->ready = 1;
+    req_dama->nombre_varon = nom;
+    local_dama = req_dama->nombre_dama;
+    pthread_cond_signal(&req_dama->w);
+  }
+  pthread_mutex_unlock(&m);
+  return local_dama;
 }
