57a3a24266b6130b34216058835c9a4ddde28c93
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-10-24 19:16:18 -0300

    t4 ready lista pa p5

--- T4/nKernel/ncompartir.c
+++ T4/nKernel/ncompartir.c
@@ -6,46 +6,60 @@
 // El descriptor de thread incluye el campo ptr para que Ud. lo use
 // a su antojo.
 
-... defina aca sus variables globales con el atributo static ...
-static int contador_th = 0;
+//... defina aca sus variables globales con el atributo static ...
+static int nAcc; // numero de threads accediendo al recurso compartido
+static NthQueue *q; // cola de threads esperando para acceder al recurso compartido
+static nThread gTh; // puntero al thread que tiene el recurso compartido
 
 // nth_compartirInit se invoca al partir nThreads para Ud. inicialize
 // sus variables globales
 
 void nth_compartirInit(void) {
-
-}
-void nCompartir(void *ptr) {
-  
   START_CRITICAL
+  q = nth_makeQueue();
+  nAcc = 0;
+  END_CRITICAL
+}
 
+void nCompartir(void *ptr) {
+  START_CRITICAL
   nThread thisTh= nSelf();
-
-
-  if (!nth_emptyQueue(thisTh->sendQueue) && contador_th != 0){
-    
+  thisTh->ptr = ptr;
+  gTh = thisTh;
+  // wake up all threads in queue
+  while(!nth_emptyQueue(q)){
+    nThread w = nth_getFront(q);
+    setReady(w);
+    schedule();
   }
-
-  
-
-
+  suspend(WAIT_ACCEDER);
+  schedule();
+  gTh->ptr = NULL;
+  gTh = NULL;
   END_CRITICAL
 }
 
 void *nAcceder(int max_millis) {
   START_CRITICAL
-
-  if (nth_emptyQueue(thisTh->sendQueue)){
-
+  nAcc++;
+  nThread thisTh= nSelf();
+  if(gTh == NULL){
+    nth_putBack(q, thisTh);
+    suspend(WAIT_COMPARTIR);
+    schedule();
   }
-
+  void * ptr = gTh->ptr;
   END_CRITICAL
+  return ptr;
 }
 
 void nDevolver(void) {
   START_CRITICAL
-
-  
-
+  nAcc--;
+  nThread thisTh= nSelf();
+  if(gTh != NULL && gTh->status == WAIT_ACCEDER && nAcc == 0){
+    setReady(gTh);
+    schedule();
+  }
   END_CRITICAL
 }
